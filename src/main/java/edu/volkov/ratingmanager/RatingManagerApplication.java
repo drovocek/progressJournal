9c35bbfd3b6fb91ecc927f6c2bb07f1c2f6574bf
drovocek <mycodepg@gmail.com>
2021-02-06 10:00:45 +0300

    init

---
+++ src/main/java/edu/volkov/ratingmanager/RatingManagerApplication.java
@@ -0,0 +1,13 @@
+package edu.volkov.ratingmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RatingManagerApplication.class, args);
+    }
+
+}
