41d349cf6acf1c759562c67d757b29e092e8eb1f
drovocek <mycodepg@gmail.com>
2021-02-06 15:20:42 +0300

    add repos

---
+++ src/main/java/edu/volkov/progressjournal/repository/JournalEntryRepository.java
@@ -0,0 +1,16 @@
+package edu.volkov.progressjournal.repository;
+
+import edu.volkov.progressjournal.model.JournalEntry;
+import edu.volkov.progressjournal.model.Subject;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+public interface JournalEntryRepository extends JpaRepository<JournalEntry, Integer> {
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM JournalEntry je WHERE je.id=:id")
+    int delete(int id);
+}
---
+++ src/main/java/edu/volkov/progressjournal/repository/SubjectRepository.java
@@ -0,0 +1,15 @@
+package edu.volkov.progressjournal.repository;
+
+import edu.volkov.progressjournal.model.Subject;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+public interface SubjectRepository extends JpaRepository<Subject, Integer> {
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Subject s WHERE s.id=:id")
+    int delete(int id);
+}
